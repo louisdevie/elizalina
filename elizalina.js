07d3426968412cdfae2a320992a89cf4257aad4b
Louis DEVIE <louisdevie.contact@gmail.com>
2022-07-18 14:18:29 +0200

    use `navigator.languages`, added the option to use a specific language … … and load the only language JSON file needed

--- elizalina.js
+++ elizalina.js
@@ -1,6 +1,6 @@
 let elizalina = {
 	__languages: [],
-	__languagesLookup: {},
+	__lookup: {},
 	__fallback: "en",
 
 	setFallback: function (langId) {
@@ -11,36 +11,62 @@ let elizalina = {
 		return this.__fallback;
 	},
 
-	loadLanguage: async function (source, targetLanguages) {
-		const response = await fetch(source);
-		const data = await response.json();
-
-		this.loadLanguageObject(data, targetLanguages);
+	addLanguage: function (source, targetIds) {
+		this.__insertLanguageData(
+			{
+				loaded: false,
+				source: source,
+			},
+			targetIds
+		);
 	},
 
-	loadLanguageObject: function (object, targetLanguages) {
-		if (!Array.isArray(targetLanguages)) {
-			targetLanguages = [targetLanguages];
+	loadLanguage: async function (source, targetIds) {
+		try {
+			const response = await fetch(source);
 		}
-
-		this.__languages.push(object);
-		const index = this.__languages.length - 1;
-
-		for (const id of targetLanguages) {
-			this.__languagesLookup[id] = index;
+		catch {
+			throw "ELIZALINA: unable to fetch <" + source + ">"
 		}
+		const data = await response.json();
+
+		this.loadLanguageObject(data, targetIds);
+	},
+
+	loadLanguageObject: function (object, targetIds) {
+		this.__insertLanguageData(
+			{
+				loaded: true,
+				data: object,
+			},
+			targetIds
+		);
 	},
 
 	isAvailable: function (langId) {
-		return this.__languagesLookup.hasOwnProperty(langId);
+		return this.__lookup.hasOwnProperty(langId);
 	},
 
-	fillDocument: function () {
-		let lang = this.__languages[this.__languagesLookup[navigator.language]];
-		let fallback = this.__languages[this.__languagesLookup[this.__fallback]];
+	fillDocument: async function (langId) {
+		let fallback = await this.__getLanguage(this.__fallback);
+		document.documentElement.lang = this.__fallback;
+
+
+		let accept = langId ? [langId] : (navigator.languages || [navigator.language]);
+
+		let userLanguage = {};
+		for (const id of accept) {
+			if (this.__lookup[id] != undefined) {
+				userLanguage = await this.__getLanguage(id);
+				document.documentElement.lang = id;
+				break;
+			}
+		}
+
+		let key, content;
 
 		for (let node of document.getElementsByClassName("elz")) {
-			let key = undefined;
+			key = undefined;
 			for (const cls of node.classList) {
 				if (cls.startsWith("_")) {
 					key = cls.substring(1);
@@ -48,15 +74,55 @@ let elizalina = {
 				}
 			}
 			if (key !== undefined) {
-				let content = lang[key] || fallback[key];
+				content = userLanguage[key] || fallback[key];
 				if (content !== undefined) {
 					node.innerText = content;
 				}
-				else
-				{
+				else {
 					node.innerText = "<missing #" + key + ">";
 				}
 			}
+		}
+	},
+
+	__insertLanguageData: function (data, targetIds) {
+		if (!Array.isArray(targetIds)) {
+			targetIds = [targetIds];
+		}
+
+		this.__languages.push(data);
+		const index = this.__languages.length - 1;
+
+		for (const id of targetIds) {
+			this.__lookup[id] = index;
+		}
+	},
+
+	__getLanguage: async function (langId) {
+		let key = this.__lookup[langId]
+
+		if (key === undefined) {
+			return {};
+		}
+		else {
+			let lang = this.__languages[key];
+
+			if (!lang.loaded) {
+				const response = await this.__fetch(lang.source);
+				lang.data = await response.json();
+				lang.loaded = true;
+			}
+
+			return lang.data;
+		}
+	},
+
+	__fetch: async function (url) {
+		try {
+			return await fetch(url);
+		}
+		catch {
+			throw "ELIZALINA: unable to fetch <" + url + ">"
 		}
 	},
 };
